4f4b236af7de9bf17dfa883083da389a4950fa01
Wan seok Park <wandol@Wanui-MacBookPro.local>
2021-01-24 03:50:48 +0900

    #2. jax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import Potato from './Potato';
+
+function Food({fav}){
+  console.log({fav});
+  return <h1>I like {fav}!</h1>
+}
 
 function App() {
   return (
     <div>
       <h1>Hello!!</h1>
-      <Potato/>
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="sam" />
+      <Food fav="chukumi" />
     </div>
   );
 }
