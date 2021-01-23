954ff68221d9abe5b774b8490bb04e1b8f1efa85
Wan seok Park <wandol@Wanui-MacBookPro.local>
2021-01-24 04:23:32 +0900

    #2 end

--- src/App.js
+++ src/App.js
@@ -1,18 +1,45 @@
 import React from "react";
+import PropType from "prop-types";
 
-function Food({fav}){
-  console.log({fav});
-  return <h1>I like {fav}!</h1>
+function Food({name, picture, rating}){
+  return (<div>
+      <h1>I like {name}!</h1>
+      <h4>{rating}</h4>
+      <img src={picture} alt={name}/>
+    </div>
+  )
+}
+
+Food.prototype = {
+  name: PropType.string.isRequired,
+  rating: PropType.number.isRequired,
+  image: PropType.string.isRequired
+}
+
+const foodILike = [
+  {
+    id: 1,
+    name:"Kimchi",
+    rating: 5,
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxn1r0L3_E3B0XVsU4CvIv0159c0ZQ0ZRTW_NMd2E6Xgh22ARK72vj-wa_uA&usqp=CAc"
+  },
+  {
+    id: 2,
+    name:"Kimchi2",
+    rating: 6,
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxn1r0L3_E3B0XVsU4CvIv0159c0ZQ0ZRTW_NMd2E6Xgh22ARK72vj-wa_uA&usqp=CAc"
+  }
+];
+
+function renderFood(dish){
+  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
 }
 
 function App() {
   return (
     <div>
       <h1>Hello!!</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="sam" />
-      <Food fav="chukumi" />
+      {foodILike.map(renderFood)}
     </div>
   );
 }
